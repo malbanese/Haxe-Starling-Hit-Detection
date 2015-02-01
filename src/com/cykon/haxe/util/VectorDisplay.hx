25c46abaf806fa9fbae822929b8033d306bb626b
Cykon <malbanese@live.com>
2015-02-01 15:32:54 -0700

    Vector display now draws arrows.
    It's easier to figure out what direction a vector is in when there is an
    arrow head rather than just a line.

--- src/com/cykon/haxe/util/VectorDisplay.hx
+++ src/com/cykon/haxe/util/VectorDisplay.hx
@@ -13,14 +13,27 @@ import starling.display.Shape;
 import com.cykon.haxe.cmath.Vector;
 
 class VectorDisplay{
-	public static function display( vector:Vector, origX:Float, origY:Float ):Shape{
+	public static function display( vector:Vector, origX:Float, origY:Float, thickness:Int = 2, color = null ):Shape{
 		var shape:Shape = new Shape();
 		
 		shape.x = origX;
 		shape.y = origY;
 		
-		shape.graphics.lineStyle(2, Math.round(Math.random()*0xFFFFFF), 1.0);
+		if(color == null)
+			color = Math.round(Math.random()*0xFFFFFF);
+		
+		shape.graphics.lineStyle(thickness, color, 1.0);
 		shape.graphics.lineTo(vector.vx, vector.vy);
+		
+		shape.graphics.beginFill(color, 1.0);
+		var v1Normal = vector.clone().normalize().multiply(5).rotate(Math.PI * 3/4);
+		shape.graphics.lineTo(vector.vx + v1Normal.vx, vector.vy + v1Normal.vy);
+		shape.graphics.moveTo(vector.vx, vector.vy);
+		
+		var v2Normal = vector.clone().normalize().multiply(5).rotate(-Math.PI * 3/4);
+		shape.graphics.lineTo(vector.vx + v2Normal.vx, vector.vy + v2Normal.vy);
+		shape.graphics.lineTo(vector.vx + v1Normal.vx, vector.vy + v1Normal.vy);
+		shape.graphics.endFill();
 		
 		return shape;
 	}
